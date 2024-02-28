ac3425751f73d9e0209c95aa32527655f5d351cd
Guilherme Romero <guiromerolima08@hotmail.com>
2024-02-28 16:00:32 -0300

    criação do projeto spring

---
+++ fiancaspro/src/test/java/com/romero/fiancaspro/FiancasproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.romero.fiancaspro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiancasproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
