082a721aabe947c7cc09a0818ed48290ce57b671
Guilherme Romero <guiromerolima08@hotmail.com>
2024-03-11 19:24:14 -0300

    add delete and put

--- fiancaspro/src/main/java/com/romero/fiancaspro/model/Categoria.java
+++ fiancaspro/src/main/java/com/romero/fiancaspro/model/Categoria.java
@@ -2,11 +2,14 @@ package com.romero.fiancaspro.model;
 
 import java.util.Random;
 
-public record Categoria (Long id , String nome , String icone){
+// imutável
+public record Categoria (Long id, String nome, String icone){
     
-    public Categoria(Long id, String nome , String icone) { 
-        this.id = Math.abs(new Random().nextLong()) ;
+    public Categoria(Long id, String nome, String icone){
+        var key = (id == null) ? Math.abs( new Random().nextLong() ) : id;
+        this.id = key;
         this.nome = nome;
         this.icone = icone;
     }
+    
 }
